1553c272741cc53bdbe510595cd30a13d2d38556
jglioz2 <95670627+jgliozzi@users.noreply.github.com>
2023-10-31 06:37:29 -0500

    Added operator and state time evolution methods using both Krylov subspace methods and Chebyshev polynomials, sped up calculation of expectation values using sparse matrices instead of np.einsum, simplified code for readability.

--- SpinChain.py
+++ SpinChain.py
@@ -1,6 +1,6 @@
 # Class for Exact Diagonalization Calculations
 import numpy as np
-from scipy import sparse
+from scipy import sparse, linalg, special
 import functools as ft
 import itertools as it
 from numba import jit
@@ -32,30 +32,19 @@ class SpinChain():
         # keep track of full hamiltonian if we project to a sector of the Hilbert space
         self.FullHam = None
 
-    # returns identity and spin matrices 
+    # returns identity and spin matrices (various forms allowed)
     # note that spin matrices by default include factor of spin (1/2 for spin-1/2, etc.)
     def getPauli(self, basis="pm"):
-        # return identity, sz, raising operator sp, lowering operator sm
-        if basis == "pm":
-            return self.spin_mat_list
-        # return identity, sx, sy, sz (spin matrices)
-        elif basis == "xyz":
-            s0, sz, sp, sm = self.spin_mat_list
-            sx = 1/2 * (sp + sm)
-            sy = 1/(2j) * (sp - sm)
-            return [s0, sx, sy, sz]
-        # return identity, X, Y, Z Pauli matrices (2 x 2)
-        elif basis == 'pauli':
-            sigmax = np.array([[0, 1], [1, 0]])
-            sigmay = np.array([[0, -1j], [1j, 0]])
-            sigmaz = np.array([[1, 0], [0, -1]])
-            return [np.eye(self.q), sigmax, sigmay, sigmaz]
-        # return identity (I) and clock (Z) and shift (X) operators 
-        elif basis == 'clock_shift':
-            w = np.exp(2*np.pi*1j/self.q)
-            Z = np.diag([w**n for n in range(self.q)])
-            X = np.roll(np.eye(self.q), -1, axis=0)
-            return [np.eye(self.q), Z, X]
+        s0, sz, sp, sm = self.spin_mat_list
+        basis_dict = {
+            "pm": [s0, sz, sp, sm], # Id, Sz, Sp, Sm (raising and lowering)
+            "xyz": [s0, 1/2 * (sp + sm), 1/(2j) * (sp - sm), sz], # Id, Sx, Sy, Sz (standard)
+            "pauli": [np.eye(2), np.array([[0, 1], [1, 0]]), 
+                        np.array([[0, -1j], [1j, 0]]), np.array([[1, 0], [0, -1]])], # Id, Sigmax, Sigmay, Sigmaz (2 x 2)
+            "clock_shift": [np.eye(self.q), np.diag([np.exp(2*np.pi*1j * n / self.q) for n in range(self.q)]), 
+                                np.roll(np.eye(self.q), -1, axis=0)] # Id, Z (clock), X (shift) 
+        }
+        return basis_dict.get(basis, [])
 
     # returns Hamiltonian (which is a sparse array)
     def getHam(self):
@@ -85,12 +74,8 @@ class SpinChain():
         else:
             @jit(nopython=True, cache=True)
             def return_answer():
-                # row vector of powers of q (decreasing)
-                powers_of_q = q**np.arange(L)[::-1]
-                # column vector of basis state indices
                 basis_arr = np.arange(q**L).reshape((-1,1))
-                # answer is (q**L, L) array
-                # row of index A is the digits of A base-q
+                powers_of_q = q**np.arange(L)[::-1]
                 return (basis_arr // powers_of_q % q)
             occ_list = return_answer()
         # subtract total spin to set range as (-spin, spin) instead of (0, 2*spin) 
@@ -99,16 +84,14 @@ class SpinChain():
     # returns magnetization and or dipole of all basis elements
     def mag_dip_basis_list(self, mag=True, dip=True):
         bto = self.basis_to_occ()
-        if mag==True and dip==False:
-            mag_basis = np.sum(bto, axis=1)
-            return mag_basis
-        elif mag==False and dip==True:
-            dip_basis = np.einsum('ij,j->i', bto, np.arange(self.L))
-            return dip_basis
-        elif mag==True and dip==True:
-            mag_basis = np.sum(bto, axis=1)
-            dip_basis = np.einsum('ij,j->i', bto, np.arange(self.L))
-            return (mag_basis, dip_basis)
+        if mag and dip:
+            mag_of_basis = np.sum(bto, axis=1)
+            dip_of_basis = np.einsum('ij,j->i', bto, np.arange(self.L))
+            return (mag_of_basis, dip_of_basis)
+        elif mag:
+            return np.sum(bto, axis=1)
+        elif dip:
+            return np.einsum('ij,j->i', bto, np.arange(self.L))
         else:
             print("SpinChain Error: either mag or dip must be True")   
     
@@ -119,6 +102,7 @@ class SpinChain():
         # e.g. ["id", "id", "0", "1", "id"] or ["2", "id", "id", "0", "1"] for PBC
         site_list = np.array(site_list) % self.L # make site list mod_L
         label_list = ["id" for site in range(self.L)]
+        
         for op_label, site in enumerate(site_list):
             label_list[site] = op_label
         # group contiguous identity operators together
@@ -194,7 +178,7 @@ class SpinChain():
         dip_list = self.mag_dip_basis_list(mag=False, dip=True)
         # indices of basis states in desired sector
         # define dipole moment mod L for periodic boundary conditions
-        if periodic_dip == True:
+        if periodic_dip:
             indices = np.where(np.abs((dip_list - Pdip)%self.L) < 1e-9)[0]
         else:
             indices = np.where(np.abs(dip_list - Pdip) < 1e-9)[0]        
@@ -211,7 +195,7 @@ class SpinChain():
         mag_list, dip_list = self.mag_dip_basis_list(mag=True, dip=True)
         # indices of basis states in desired sector
         # project to Pdip (mod L) if we have PBC because shifting dipole is only well-defined mod L (and with M=0)
-        if periodic_dip == True:
+        if periodic_dip:
             indices = np.where(np.abs(mag_list - M) + np.abs((dip_list - Pdip)%self.L) < 1e-9)[0]
         else:
             indices = np.where(np.abs(mag_list - M) + np.abs(dip_list - Pdip) < 1e-9)[0]
@@ -265,7 +249,7 @@ class SpinChain():
     def getEntEntropy(self, state, region, n=1, ent_spectrum=False):
         # put state(s) into full Hilbert space
         num_states = 1 if len(state.shape)==1 else state.T.shape[0]
-        if self.projector != None:
+        if self.projector is not None:
             state = self.projector.T @ state
 
         # reshape to num_states x q x q x .... x q (one axis of length q per site)
@@ -280,7 +264,7 @@ class SpinChain():
         state_mat = np.reshape(np.transpose(psi, [0] + regionA + regionB), (num_states, self.q**len(regionA), -1))
         svals = np.linalg.svd(state_mat, compute_uv=False)
         
-        if ent_spectrum == True:
+        if ent_spectrum:
             return svals**2 if num_states > 1 else (svals**2)[0]
         else:
             if n == 1:
@@ -321,26 +305,31 @@ class SpinChain():
     def getIPR_LR(self, stateL, stateR, q=2):
         return np.sum(np.abs(stateL.conj() * stateR)**q, axis=0)
 
+    # local expecation value of generic operator in one or many states (columns)
+    def getExpectationValue(self, state, op_list, site_list):
+        num_states = 1 if len(state.shape)==1 else state.T.shape[0]
+
+        op = self.op_at_sites(op_list, site_list)
+        if self.projector is not None:
+            op = self.projector @ op @ self.projector.T
+
+        expval = state.conj().T @ op @ state
+        if num_states > 1:
+            expval = np.diag(expval)
+
+        return expval.real
+
     # gives list of local spins <Sz> on each lattice site (which is index)
-    # for input of many states, output is indexed: row = state, col=positions
+    # for input of many states (cols), output is indexed: row = state, col=positions
     def getLocalSpin(self, state):
         positions = []
         sz = self.spin_mat_list[1]
+        
         for i in range(self.L):
-            sz_i = self.op_at_sites([sz], [i])
-            if self.projector != None:
-                sz_i = self.projector @ sz_i @ self.projector.T
-            eval_i = np.einsum('...a,ab,b...->...', state.conj().T, sz_i.toarray(), state).real
+            eval_i = self.getExpectationValue(state, [sz], [i])
             positions.append(eval_i)
+        
         return np.array(positions).T  
-
-    # local expecation value of generic operator in one or many states
-    def getExpectationValue(self, state, op_list, site_list):
-        op = self.op_at_sites(op_list, site_list)
-        if self.projector != None:
-            op = self.projector @ op @ self.projector.T
-        expval = np.einsum('...a,ab,b...->...', state.conj().T, op.toarray(), state).real
-        return expval
 
     # returns ground state of Hermitian Hamiltonian
     def getGroundState(self):
@@ -365,11 +354,11 @@ class SpinChain():
         # project back to full Hilbert space if we are in symm sector
         # also keep track of how many states are outside of overall symmetry sector
         num_outside = 0
-        if self.projector != None:
+        if self.projector is not None:
             labels = self.projector.T @ labels
             num_outside = np.diff(self.projector.shape)[0]
         
-        if states == True:
+        if states:
             # make list of ordered pairs (state_index, sector_label)
             labels = np.column_stack( (np.arange(self.q**self.L), labels) )
             # sort by increasing sector label
@@ -405,12 +394,13 @@ class SpinChain():
         result = sparse.linalg.expm_multiply(-1.j*time*self.Ham.tocsc(), state)
         return result/np.linalg.norm(result)
     
-    # Chebyshev approximation to time evolution at order 'order'
-    # more controlled for long times, only works for Hermitian H
-    # import E_min, E_max, and H_tilde (rescaled hamiltonian) if possible
-    def timeEvolveCheb(self, state, time, order, E_range=None, H_tilde=None):
+    # Chebyshev approximation to time evolution at certain order (only Hermitian H)
+    # optionally import E_min, E_max, and H_tilde (rescaled hamiltonian) if possible
+    # optionally compute expectation value of operator w/o computing state
+    def timeEvolveCheb(self, state, time, order, E_range=None, H_tilde=None, op_list=None, site_list=None):
+        D = self.getHilbertDim()
         # import E_min and E_max of Hamiltonian if known, recalculate otherwise
-        if E_range == None:
+        if E_range is None:
             E_min = sparse.linalg.eigsh(self.Ham, k=1, which='SA', return_eigenvectors=False)[0]
             E_max = sparse.linalg.eigsh(self.Ham, k=1, which='LA', return_eigenvectors=False)[0]
         else:
@@ -422,34 +412,111 @@ class SpinChain():
         b = (E_max + E_min)/2
 
         # rescaled Hamiltonian to have spectrum in [-1, 1] (import if possible)
-        if H_tilde == None:
-            H_tilde = (self.Ham - b * sparse.eye(self.Ham.shape)) / a          
-        # linear operator allows for faster matrix vector multiplication    
-        H_tilde_op = sparse.linalg.LinearOperator((self.Ham.shape, self.Ham.shape), 
-                                                    matvec=lambda x: H_tilde @ x)
+        if H_tilde is None:
+            H_tilde = (self.Ham - b * sparse.eye(D)) / a          
+        H_tilde_op = sparse.linalg.LinearOperator((D, D), matvec=lambda x: H_tilde @ x)
 
-        # set coefficients of Chebyshev expansion with bessel functions
-        coeffs = 2 * (-1j)**np.arange(cheb_order) * special.jv(np.arange(cheb_order), a*t)
-        coeffs[0] /= 2 # first coefficient has extra factor of 1/2
+        # set coefficients of Chebyshev expansion
+        coeffs = 2 * (-1j)**np.arange(order) * special.jv(np.arange(order), a*time)
+        coeffs[0] /= 2 
         
-        # first two chebyshev states
-        m0 = psi_init
-        m1 = H_tilde_op.matvec(m0) 
-
-        # recursively create more chebyshev states and add w/ coeffs to get result
-        result = coeffs[0]*m0 + coeffs[1]*m1
-        for n in range(2, cheb_order):
-            # define next vector recursively based on previous two
+        # create states for Chebyshev expansion (these are column states)
+        cheb_states = np.empty((D, order), dtype=complex)
+        m0 = state
+        m1 = H_tilde_op.matvec(m0)
+        cheb_states[:, 0] = m0
+        cheb_states[:, 1] = m1
+        # recursively create rest of Chebyshev states
+        for n in range(2, order):
             m_next = 2 * H_tilde_op.matvec(m1) - m0
-            # add to series approximation
-            result += coeffs[n] * m_next
-            # update previous two vectors in recursive series
+            cheb_states[:, n] = m_next
             m0, m1 = m1, m_next
+        
+        # compute state |psi(t)>
+        if op_list is None:
+            result = np.einsum('i,ji->j', coeffs, cheb_states) 
+            result *= np.exp(-1.j * (E_min + a * (1-eps/2)) * time) 
+            return result/np.linalg.norm(result)
+        # compute expectation value of operator <psi(t)|O|psi(t)>
+        else:
+            op_full = self.op_at_sites(op_list, site_list) 
+            coeff_mat = np.einsum('a,b -> ab', coeffs.conj(), coeffs)
+            op_cheb = cheb_states.T.conj() @ op_full @ cheb_states
+            return np.sum(coeff_mat * op_cheb).real
 
-        # multiply result by time dependent phase
-        result *= np.exp(-1.j * (E_min + a * (1-eps/2)) * t)
+    # time evolves state using Krylov subspace method (only Hermitian H)
+    # max_states is maximum size of Krylov subspace, tol sets convergence criterion
+    # optionally compute expectation value of operator w/o computing state
+    def timeEvolveKrylov(self, state, time, max_states, tol=1e-3, op_list=None, site_list=None):
+        D = self.getHilbertDim()
+        Ham_op = sparse.linalg.LinearOperator((D, D), matvec=lambda x: self.Ham @ x) # faster mat-vec product
+        
+        # Applies hamiltonian to krylov subspace (columns) and generates new vector 
+        # returns orthonormalized updated krylov subspace with extra vector
+        def applyH_ortho(Ham_op, prev_states):
+            new_state = Ham_op.matvec(prev_states[:, -1]) # apply H
+            orth_states, _ = np.linalg.qr( np.column_stack((prev_states, new_state)) ) # orthogonalize ALL states
+            return orth_states
 
-        return result/np.linalg.norm(result)
+        # computes (forced) tridiagonal hamiltonian projected into krylov subspace
+        # only works for Hermitian H ! (need to implement Arnoldi algorithm for non-herm)
+        def compute_effectiveH(Heff, Ham_op, states):
+            last_state = states[:, -1]
+            penultimate_state = states[:, -2]
+            
+            ket = Ham_op.matvec(last_state) # apply H only to newest state
 
+            alpha = np.dot(last_state.conj(), ket).real # diagonal component
+            beta = np.dot(penultimate_state.conj(), ket).real # off_diagonal component
 
+            # add 1 new diagonal and 1 super + sub diagonal elements
+            Heff_new = np.pad(Heff, [(0, 1), (0, 1)], mode='constant')
+            Heff_new[-1, -1] = alpha
+            Heff_new[-1, -2], Heff_new[-2, -1] = beta, beta
 
+            return Heff_new
+
+        # Now perform the actual evolution
+        v0 = state/np.linalg.norm(state)
+        print(v0.shape)
+        Heff = np.dot(v0, Ham_op.matvec(v0)).reshape((1,1)).real # first effective hamiltonian is 1 x 1
+        krylov_states = v0.reshape((-1,1)) # make into column vector
+
+        c_previous = np.array([1]) # compare successive iterations to see if state converges before max_states
+        # iteratively generate krylov subspace
+        for j in range(1, max_states):
+            krylov_states = applyH_ortho(Ham_op, krylov_states) # D x (j+1) mat (j+1 vectors)
+            Heff = compute_effectiveH(Heff, Ham_op, krylov_states) # (j+1) x (j+1) mat
+
+            # unit vector in krylov basis
+            e = np.zeros(j+1) 
+            e[0] = 1 
+
+            # time evolve state coefficients c in small Krylov basis
+            if j < 90: 
+                c = linalg.expm(-1.j*time*Heff) @ e
+            else:
+                c = sparse.linalg.expm_multiply(-1.j*time*sparse.csr_array(Heff), e)
+            
+            diff_coeff = c[:-1] - c_previous # compute Hilbert space distance between successive iterations
+            distance = np.linalg.norm(diff_coeff)**2 + np.abs(c[-1])**2 # add newest coeff in separately
+            if distance < tol:
+                print(f"Converged using Krylov subspace of {j+1} states")
+                break
+            else:
+                c_previous = c
+
+        # compute state |psi(t)>
+        if op_list is None:
+             # project back into full Hilbert space to get final state
+            state_final = np.linalg.norm(state) * np.einsum('i,ji->j', c, krylov_states)
+            state_final /= np.linalg.norm(state_final)
+            return state_final
+        # compute expectation value of operator <psi(t)|O|psi(t)> 
+        else:
+            op_full = self.op_at_sites(op_list, site_list) 
+            coeff_mat = np.einsum('a,b->ab', c.conj(), c) 
+            op_krylov = krylov_states.T.conj() @ op_full @ krylov_states 
+            return np.sum(coeff_mat * op_krylov).real
+
+        
